6202464cd42ab3c5ba8e60faa0e5823274118c2b
iamngoni <imngonii@gmail.com>
2023-01-24 13:07:04 +0000

    fix: remove unnecessary permissions

--- services/permissions/is_patient.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-from users.models import UserRoles
-
-
-class IsPatient(BasePermission):
-    message = "You must be a patient to access this resource."
-
-    def has_permission(self, request, view):
-        return request.user.role == UserRoles.PATIENT
