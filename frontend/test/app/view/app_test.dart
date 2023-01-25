8c988323e360c8077d18b5f9e01071fe6fdd6cbe
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-01-25 09:38:15 +0200

    fix: test

--- frontend/test/app/view/app_test.dart
+++ frontend/test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/app/app.dart';
-import 'package:frontend/features/calculator/views/pages/calculator.dart';
+import 'package:frontend/features/calculator/views/pages/home.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CalculatorPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CalculatorPage), findsOneWidget);
+      expect(find.byType(CalculatorHome), findsOneWidget);
     });
   });
 }
